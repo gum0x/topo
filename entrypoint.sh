86e970743f0ba03a32b7bbb2f98935ffb167585f
Jose Moyano <jmoyano@a2secure.com>
2019-03-24 23:27:55 +0100

    - Added loop as entry point because socat tunnel is restarted often

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo openssl:"$SOCAT_HOST":"$SOCAT_PORT":verify=0,ignoreeof && /usr/sbin/tcpdump -i ${IFACE} -w - "($PCAP_FILTER) and not (dst host $SOCAT_HOST and dst port $SOCAT_PORT)"| socat - openssl:"$SOCAT_HOST":"$SOCAT_PORT",verify=0,ignoreeof
+while true; do
+	echo "Starting tcpdump and tunnel in $(hostname)"
+	echo openssl:"$SOCAT_HOST":"$SOCAT_PORT":verify=0,ignoreeof && \
+	/usr/sbin/tcpdump -i ${IFACE} -w - "($PCAP_FILTER) and not (dst host $SOCAT_HOST and dst port $SOCAT_PORT)"| \
+       	socat - openssl:"$SOCAT_HOST":"$SOCAT_PORT",verify=0,ignoreeof
+	echo "Tunnel stopped in $(hostname)"
+	sleep 1
+done
